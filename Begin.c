13853ef450136859cf2df0885b0aa684090c6c01
Zachary Klagge <zklagge@fastenal.com>
2022-10-27 13:20:32 -0500

    I think this works well enough for now

---
+++ Begin.c
@@ -0,0 +1,14 @@
+/*First attempt at the glorious C language*/
+
+#include <stdio.h>
+
+int main()
+{
+    /*
+    *Output "Once more the Sith will rule the Galaxy!"
+    *Using the C standard library printf function
+    *printf defined in the header file
+    */
+   printf("Once more the Sith will rule the Galaxy!");
+   return 0;
+}
