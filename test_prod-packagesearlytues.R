56abfcb971542026d14cb6c44f9cc3d302ede2d8
ZacharyKlagge <zklagge@fastenal.com>
2023-06-27 14:23:48 -0500

    Relatively good progress?

--- test_prod-packagesearlytues.R
+++ test_prod-packagesearlytues.R
@@ -10,23 +10,23 @@ if(!require(ggplot2)) {
 }
 
 # Define the objective function coefficients
-f.obj <- c(0.11, 0.264, 0.417)
+f.obj <- c(0.11, 0.15, 0.264062)
 
 # Define the constraint matrix (how many units per each, respectively)
-f.con <- matrix(c(52, 230, 924), nrow = 1)
+f.con <- matrix(c(43, 100, 229), nrow = 1) #Assuming the P3-4 hybrid volume is about 43 cubic inches
 
 # Define the direction of the constraints (do we want to consider partial fill)
 f.dir <- "="
 
 # Define the demand range (simulation range)
-demand_range <- 1:1000
+demand_range <- 1:750
 
 # Initialize vectors to store the results
 total_cost <- numeric(length(demand_range))
 package_type <- character(length(demand_range))
 
 # Define the package names (Medium/Golden/Minibulk, etc?)
-package_names <- c("Small", "Medium", "Large")
+package_names <- c("P4", "Golden", "C6")
 
 # Loop over the demand values
 for (i in seq_along(demand_range)) {
@@ -54,6 +54,6 @@ results <- data.frame(Demand = demand_range, TotalCost = total_cost, PackageType
 # Plot the results
 ggplot(results, aes(x = Demand, y = TotalCost, color = PackageType)) +
   geom_line() +
-  labs(title = "Optimal Packaging Solution", x = "Number of Objects Ordered", y = "Total Cost") +
-  scale_color_discrete(name = "Package Type")
+  labs(title = "Optimal Packaging Solution", x = "Cubic Volume", y = "Total Cost ($)") +
+  scale_color_discrete(name = "Pack Type")
 
